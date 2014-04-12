e06424c1be3fdefb7e8fa7fc6bc68dd587f09e5e
Andrew Smillie <ajsmillie@gmail.com>
2014-04-12 23:26:08 +0100

    Tick the received state

--- app/controllers/Simulator.java
+++ app/controllers/Simulator.java
@@ -9,8 +9,8 @@ import model.LifeSupport;
 public class Simulator {
 
 	@Expose private Colony colony;
-	@Expose private Environment environment;
-	@Expose private LifeSupport lifeSupport;
+//	@Expose private Environment environment;
+//	@Expose private LifeSupport lifeSupport;
 	
 	public Colony getColony() {
 		return colony;
@@ -18,24 +18,24 @@ public class Simulator {
 	public void setColony(Colony colony) {
 		this.colony = colony;
 	}
-	public Environment getEnvironment() {
-		return environment;
-	}
-	public void setEnvironment(Environment environment) {
-		this.environment = environment;
-	}
-	public LifeSupport getLifeSupport() {
-		return lifeSupport;
-	}
-	public void setLifeSupport(LifeSupport lifeSupport) {
-		this.lifeSupport = lifeSupport;
-	}
-	
-	public Simulator(Colony colony, Environment environment, LifeSupport lifeSupport){
-		this.colony = colony;
-		this.environment = environment;
-		this.lifeSupport = lifeSupport;
-	}
+//	public Environment getEnvironment() {
+//		return environment;
+//	}
+//	public void setEnvironment(Environment environment) {
+//		this.environment = environment;
+//	}
+//	public LifeSupport getLifeSupport() {
+//		return lifeSupport;
+//	}
+//	public void setLifeSupport(LifeSupport lifeSupport) {
+//		this.lifeSupport = lifeSupport;
+//	}
+//	
+//	public Simulator(Colony colony, Environment environment, LifeSupport lifeSupport){
+//		this.colony = colony;
+//		this.environment = environment;
+//		this.lifeSupport = lifeSupport;
+//	}
 	
 	public static Simulator tick(Simulator simulator, int steps) {
 		simulator.colony.tick(steps);
