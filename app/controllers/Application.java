4528f59a2af78d1becc425aceb745c81bb9fa74e
Andrew Smillie <ajsmillie@gmail.com>
2014-04-12 17:23:17 +0100

    CORS enable

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,32 +1,29 @@
 package controllers;
 
-import play.*;
-import play.api.libs.json.Json;
-import play.mvc.*;
-import views.html.*;
+import java.util.List;
+
 import model.Placeable;
-import model.components.AeroponicsGrowingBay;
-import model.components.Component;
 import model.components.ComponentFactory;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
-    
-    public static Result getComponents() {
-    	 Gson gson = new Gson();
-    	 gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
-    	 List<Placeable> components = ComponentFactory.listAll();
-    	 return ok(gson.toJson(components));
-    }
+	public static Result index() {
+		return ok(index.render("Your new application is ready."));
+	}
+
+	public static Result getComponents() {
+		response().setHeader("Access-Control-Allow-Origin", "*");
+
+		Gson gson = new Gson();
+		gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+		List<Placeable> components = ComponentFactory.listAll();
+		return ok(gson.toJson(components));
+	}
 
 }
